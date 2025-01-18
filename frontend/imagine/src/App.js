81430b04a4bbd197bdebca896a8e9badec7f90c6
varshaislur <varshaislur@gmail.com>
2025-01-18 14:46:28 +0530

    react router added

--- frontend/imagine/src/App.js
+++ frontend/imagine/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import './App.css';
 
+import HomePage from "./pages/HomePage";
+
 
 function App() {
 
@@ -8,29 +10,15 @@ function App() {
   return (
     <div className="App">
       
-       <ThemeProvider theme={fontcolorTheme}>
-        <UsernameProvider>
-       <EmailProvider>
+      
+        
          <Router> 
           <Routes>
-            <Route path="/scoperecommendations" element={<ScopeReco />} /> 
-            <Route path="/materialrecommendations" element={<Recommendations/>} /> 
-            <Route path="/teacherDashboard" element={<TeacherDashboard />} />
-            <Route path="/studentDashboard" element={<StudentDashboard />} />
-            <Route path="/addstudents" element={<AddStudents />} />
-            <Route path="/addmaterial" element={<AddMaterial/>} />
-            <Route path="/quiz" element={<Quiz/>} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/loginTeacher" element={<LoginTeacher/>} />
-            <Route path="/signupTeacher" element={<SignUpTeacher/>} />
-            <Route path="/loginStudent" element={<LoginStudent/>} />
-            <Route path="/signupStudent" element={<SignUpStudent/>} />
-            <Route path="/" element={<Home />} />
+            
+            <Route path="/" element={<HomePage />} />
           </Routes>
         </Router>
-        </EmailProvider>
-        </UsernameProvider>
-        </ThemeProvider>
+       
         
     </div>
   );
